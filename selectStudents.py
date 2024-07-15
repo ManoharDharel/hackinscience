9d8755394d5d64ccdafa39f0d379855383c1cd54
manoh <manohardhare@gmail.com>
2024-07-15 22:06:38 +1000

    wdwdw

---
+++ selectStudents.py
@@ -0,0 +1,7 @@
+def select_students(student_list, grade):
+    my_class = [['Kermit Wade', 27], ['Hattie Schleusner', 67], ['Ben Ball', 5], ['William Lee', 2]]
+    return my_class
+
+print(select_students())
+
+

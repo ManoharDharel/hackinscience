bb17a565160bc609d761d653ad2ba2c63aeed1b5
manoh <manohardhare@gmail.com>
2024-07-15 23:02:01 +1000

    finally made it

--- selectStudents.py
+++ selectStudents.py
@@ -1,7 +1,18 @@
-def select_students(student_list, grade):
-    my_class = [['Kermit Wade', 27], ['Hattie Schleusner', 67], ['Ben Ball', 5], ['William Lee', 2]]
-    return my_class
-
-print(select_students())
+my_class = [['Kermit Wade', 27], ['Hattie Schleusner', 67], ['Ben Ball', 5], ['William Lee', 2]]
 
 
+def select_student(students, threshold):
+    accepted = []
+    refused = []
+    for item in students:
+        if item[1] >= threshold:
+            accepted.append(item)
+        else:
+            refused.append(item)
+
+    value_item = {"Accepted": sorted(accepted, key=lambda x: x[1], reverse=True),
+                  "Refused": sorted(refused, key=lambda x: x[1])}
+    return value_item
+
+
+print(select_student(my_class, 20))
